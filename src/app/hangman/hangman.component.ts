a9d158fd55b534f3dea6685c9c9741e5b45243f2
Justus M <justusmrosk@web.de>
2023-11-28 12:54:41 +0100

    added Game Logic

--- src/app/hangman/hangman.component.ts
+++ src/app/hangman/hangman.component.ts
@@ -2,20 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HangmanService } from '../hangman.service';
 import {InputComponent} from "../input/input.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hangman',
   standalone: true,
-  imports: [CommonModule, InputComponent],
+  imports: [CommonModule, InputComponent, FormsModule],
   templateUrl: './hangman.component.html',
   styleUrl: './hangman.component.scss'
 })
 export class HangmanComponent {
 
+  inputChar: string = "";
 
 
   constructor(public service: HangmanService) {
-
   }
 
 
