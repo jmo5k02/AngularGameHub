dc13bf24c738cf9d309a40ded1aa019bc8a634b7
Justus M <justusmrosk@web.de>
2023-11-29 16:13:13 +0100

    added collision and eating

---
+++ src/app/snake/entities/eatables/apple.ts
@@ -0,0 +1,33 @@
+import { Board } from "../board/board";
+import { Eatable } from "./eatable";
+
+export class Apple implements Eatable {
+    posX: number;
+    posY: number;
+
+    nutrition: number = 1;
+
+    constructor(x: number, y: number) {
+        this.posX = x;
+        this.posY = y;
+    }
+
+    spawn(board: Board): number[] {
+        let x = Math.floor(Math.random() * board.boardSize);
+        let y = Math.floor(Math.random() * board.boardSize);
+
+        while (board.board[x][y] !== 0) {
+            x = Math.floor(Math.random() * board.boardSize);
+            y = Math.floor(Math.random() * board.boardSize);
+        }
+
+        this.posX = x;
+        this.posY = y;
+
+        return [x, y];
+    }
+
+    getPosition(): number[] {
+        return [this.posX, this.posY];
+    }
+}
