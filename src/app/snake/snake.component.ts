578c32d58fdb132410e22247c02cf574e886a48c
unknown <antoni.nico97@gmail.com>
2023-11-29 14:52:36 +0100

    fixes and pulled origin stage

--- src/app/snake/snake.component.ts
+++ src/app/snake/snake.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardComponent } from './components/board/board.component';
 import { FormsModule } from '@angular/forms';
+import {UiComponent} from "./components/ui/ui.component";
 
 @Component({
   selector: 'app-snake',
   standalone: true,
-  imports: [CommonModule, BoardComponent, FormsModule],
+  imports: [CommonModule, BoardComponent, FormsModule, UiComponent],
   templateUrl: './snake.component.html',
   styleUrl: './snake.component.scss'
 })
