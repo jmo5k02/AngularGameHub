befb9fe2a166c96662247176c34a5d9d67cdb862
unknown <antoni.nico97@gmail.com>
2023-11-29 14:21:44 +0100

    added KeyListener in ui-component

--- src/app/snake/snake.component.ts
+++ src/app/snake/snake.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {UiComponent} from "./components/ui/ui.component";
 
 @Component({
   selector: 'app-snake',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UiComponent],
   templateUrl: './snake.component.html',
   styleUrl: './snake.component.scss'
 })
