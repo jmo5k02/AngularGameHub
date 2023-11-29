eaad3044a6c57becb52b2629608bfefa01acfb79
Justus M <justusmrosk@web.de>
2023-11-29 11:48:07 +0100

    added ui for snake

---
+++ src/app/snake/components/ui/ui.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ui',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ui.component.html',
+  styleUrl: './ui.component.scss'
+})
+export class UiComponent {
+
+}
